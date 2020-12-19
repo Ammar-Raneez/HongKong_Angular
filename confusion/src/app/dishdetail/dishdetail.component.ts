7ccade1a944c611c69d208b4dd0c3cdea566ea13
Ammar <ammarraneez@gmail.com>
2020-12-19 19:21:36 +0530

    promises part 2

--- confusion/src/app/dishdetail/dishdetail.component.ts
+++ confusion/src/app/dishdetail/dishdetail.component.ts
@@ -16,7 +16,7 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
-    this.dishservice.getDish(id)
+    this.dishservice.getDish("" + id)
       .then(dish => this.dish = dish)
   }
 

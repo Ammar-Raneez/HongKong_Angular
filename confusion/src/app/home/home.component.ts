c4e41e0995790e44677ed36df8bbc3e9395ecb57
Ammar <ammarraneez@gmail.com>
2020-12-19 16:28:14 +0530

    week 2 assignment

--- confusion/src/app/home/home.component.ts
+++ confusion/src/app/home/home.component.ts
@@ -1,22 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { DishService } from '../services/dish.service';
-import { PromotionService } from '../services/promotion.service';
+import { Component, OnInit, Inject } from '@angular/core';
+
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
 import { Promotion } from '../shared/promotion';
+import { PromotionService } from '../services/promotion.service';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  host: {
+    '[@flyInout]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class HomeComponent implements OnInit {
-  dish: Dish;
-  promotion: Promotion;
 
-  constructor(private dishservice: DishService, private promotionservice: PromotionService) { }
+  dish: Dish;
+  dishErrMess: string;
+  promotion: Promotion;
+  leader: Leader;
+
+  constructor(private dishService: DishService,
+    private promotionService: PromotionService,
+    private leaderService: LeaderService,
+    @Inject('BaseURL') private BaseURL ) { }
 
   ngOnInit() {
-    this.dish = this.dishservice.getFeaturedDish();
-    this.promotion = this.promotionservice.getFeaturedPromotion();
+    this.dishService.getFeaturedDish()
+      .subscribe(dish => this.dish = dish,
+        errmess => this.dishErrMess = <any>errmess);
+    this.promotionService.getFeaturedPromotion()
+    .subscribe(promotion => this.promotion = promotion);
+    this.leaderService.getFeaturedLeader()
+      .subscribe(leader => this.leader = leader);
   }
+
 }

f036a5c815930c661a327fb0331a302bbbc316b4
Ammar <ammarraneez@gmail.com>
2020-12-20 10:44:57 +0530

    Rxjs part 2

--- confusion/src/app/shared/dish.ts
+++ confusion/src/app/shared/dish.ts
@@ -2,13 +2,13 @@ import { Comment } from './comment';
 //we're gonna use this template in order to construct dishes
 //n we'll need to specify all these properties for any dish we create
 export class Dish {
-    id: String;
-    name: String;
-    image: String;
-    category: String;
+    id: string;
+    name: string;
+    image: string;
+    category: string;
     featured: boolean;
-    label: String;
-    price: String;
-    description: String;
+    label: string;
+    price: string;
+    description: string;
     comments: Comment[];
 }
